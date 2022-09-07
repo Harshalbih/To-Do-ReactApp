4f6e00b94dd44d18a45bcdacb5b2ae9adbb994a9
Harshal <bihadeharsh@gmail.com>
2022-09-07 23:28:49 +0530

    Updates

---
+++ precache-manifest.b25f4625521e1aa5cc75b156090e68c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7927072493ccdccd84d",
+    "url": "/To-Do-ReactApp/static/css/main.3b77314b.chunk.css"
+  },
+  {
+    "revision": "e7927072493ccdccd84d",
+    "url": "/To-Do-ReactApp/static/js/main.e7927072.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/To-Do-ReactApp/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "cb1335819c6f651c03ba",
+    "url": "/To-Do-ReactApp/static/js/2.cb133581.chunk.js"
+  },
+  {
+    "revision": "b6fc0e2417ebbbb96cda",
+    "url": "/To-Do-ReactApp/static/js/runtime~main.b6fc0e24.js"
+  },
+  {
+    "revision": "468cffdd9c5d18be99266602e9806f0c",
+    "url": "/To-Do-ReactApp/index.html"
+  }
+];
